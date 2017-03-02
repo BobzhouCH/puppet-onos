f36a99fb7f8eda83c7d0d738ea89a92ffae9c4c4
bobzhouHW <bob.zh@huawei.com>
2017-03-02 17:27:13 +0800

    update start.pp

--- manifests/start.pp
+++ manifests/start.pp
@@ -1,3 +1,20 @@
+# comment neutron related steps
+#
+#
+#exec{ 'Configure Neutron3':
+#        command  => "mysql -e 'drop database if exists neutron;';
+#                    mysql -e 'create database neutron character set utf8;';
+#                    mysql -e \"grant all on neutron.* to 'neutron'@'%';\";
+#                    neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head;",
+#} ->
+#
+#        exec{ 'restart neutron':
+#        command  => "service neutron-server restart",
+#}
+#
+#
+#
+
 class onos::start($onos_ip){
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
@@ -6,17 +23,6 @@ neutron_plugin_ml2 {
   'ml2_onos/username':           value => 'admin';
   'ml2_onos/url_path':           value => "http://$onos_ip:8181/onos/vtn";
 }
-
-exec{ 'Configure Neutron3':
-        command  => "mysql -e 'drop database if exists neutron;';
-                    mysql -e 'create database neutron character set utf8;';
-                    mysql -e \"grant all on neutron.* to 'neutron'@'%';\";
-                    neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head;",
-} ->
-
-        exec{ 'restart neutron':
-        command  => "service neutron-server restart",
-}
-
+include ::neutron::deps
 }
 
