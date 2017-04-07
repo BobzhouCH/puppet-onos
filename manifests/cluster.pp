809385b986d228caecaa11af8850a0ca3d566d9b
bobzhouHW <bob.zh@huawei.com>
2017-04-07 10:20:17 +0800

    cluster.pp and service.pp

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -1,17 +1,20 @@
 class onos::cluster ($controllers_ip) {
 
 ## create onos cluster
-if size($controllers_ip) > 1 and !$onos_cluster_file_path {
   $ip1 = $controllers_ip[0]
   $ip2 = $controllers_ip[1]
   $ip3 = $controllers_ip[2]
   exec{ 'create onos cluster':
-        command => "/opt/onos/bin/onos-form-cluster $ip1 $ip2 $ip3"
+        command => "/opt/onos/bin/onos-form-cluster $ip1 $ip2 $ip3",
+		onlyif => "test ! -f $onos_cluster_file_path",
+		path => "/usr/bin:/usr/sbin:/bin:/sbin",
+		logoutput => "true",
+		timeout => 30
   }->
   exec{ 'sleep 150 for onos restart':
         command => 'sudo sleep 150',
 		path => "/usr/bin:/usr/sbin:/bin:/sbin",
-        logoutput => "true",
+		onlyif => "test ! -f $onos_cluster_file_path",
+		timeout => 150
   }
-}
 }
