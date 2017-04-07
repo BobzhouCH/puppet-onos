a770f486241167b8ea02903bed257f7b5de114ce
bobzhouHW <bob.zh@huawei.com>
2017-04-07 15:12:31 +0800

    update cluster.pp

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -8,13 +8,14 @@ class onos::cluster ($controllers_ip) {
         command => "/opt/onos/bin/onos-form-cluster $ip1 $ip2 $ip3",
 		onlyif => "test ! -f $onos_cluster_file_path",
 		path => "/usr/bin:/usr/sbin:/bin:/sbin",
+		notify => Notify['onos cluster set up'],
 		logoutput => "true",
 		timeout => 30
   }->
-  exec{ 'sleep 150 for onos restart':
-        command => 'sudo sleep 150',
+  exec{ 'sleep 60 for onos restart':
+        command => 'sudo sleep 60',
 		path => "/usr/bin:/usr/sbin:/bin:/sbin",
-		onlyif => "test ! -f $onos_cluster_file_path",
-		timeout => 150
+		timeout => 60
   }
+  notify {'onos cluster set up': }
 }
